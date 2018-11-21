a7ec9c57f7f42744d4c002d56c02548d4a7d80e6
darryl.beronque <darryl.beronque@accenture.com>
2018-11-21 08:08:20 -0600

    Initial Commit

---
+++ SpeedTextingTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeedTextingTest
+//
+//  Created by darryl.beronque on 11/21/18.
+//  Copyright © 2018 darrylberonque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
